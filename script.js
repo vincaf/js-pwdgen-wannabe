7fd36deb4687e7d8a2fd241485978adc4d65f79b
vincaf <vincenzocafassog@gmail.com>
2022-06-13 14:53:36 +0200

    Insert html header,main + initialized variables js

---
+++ script.js
@@ -0,0 +1,9 @@
+const userName = prompt('Inserisci il tuo nome');
+
+const userLastName = prompt('Inserisci il tuo nome');
+
+const userFavoriteColour = prompt('Inserisci il tuo nome');
+
+let currentYear = 2022;
+
+
