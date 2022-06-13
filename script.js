3066dd75edc086708d555aec5927ecf33330f8b9
vincaf <vincenzocafassog@gmail.com>
2022-06-13 15:03:34 +0200

    JS script user information entered

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+// Inizializzazione delle variabili
+
 const userName = prompt('Inserisci il tuo nome');
 
-const userLastName = prompt('Inserisci il tuo nome');
+const userLastName = prompt('Inserisci il tuo cognome');
 
-const userFavoriteColour = prompt('Inserisci il tuo nome');
+const userFavoriteColour = prompt('Inserisci il tuo colore preferito');
 
 let currentYear = 2022;
 
+// Verifica su console dei dati assegnati
 
+console.log(userName, userLastName, userFavoriteColour, currentYear);
+
+// Scrittura su pagina browser
+
+document.getElementById('userName').innerHTML = userName;
+
+document.getElementById('userLastName').innerHTML = userLastName;
+
+document.getElementById('userFavoriteColour').innerHTML = userFavoriteColour;
+
+document.getElementById('currentYear').innerHTML = currentYear;
+
+document.getElementById('password').innerHTML = userName + userLastName + userFavoriteColour + "22";
